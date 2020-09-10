76e2f6bf8add2c9fc36ba8d46619a1c5f124863e
Josh Dunson <jdunson98@yahoo.com>
2020-09-10 11:08:46 -0500

    met mvp

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,45 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("item enhancement tests", () => {
+    it("repairs", () => {
+        expect(enhancer.repair({
+            name: "Sword",
+            durability: 20,
+            enhancement: 15
+        })
+        ).toEqual({
+            name: "Sword",
+            durability: 100,
+            enhancement: 15
+        })
+    })
+
+    it("successfully enhances", () => {
+        expect(enhancer.success({
+            name: "Mace",
+            durability: 55,
+            enhancement: 14
+        })
+        ).toEqual({
+            name: "Mace",
+            durability: 55,
+            enhancement: 15
+        })
+    })
+    
+    it("fails to enhance", () => {
+        expect(enhancer.fail({
+            name: "Staff",
+            enhancement: 18,
+            durability: 100
+        })
+        ).toEqual({
+            name: "Staff",
+            enhancement: 17,
+            durability: 90
+        })
+    })
+})
+
+
